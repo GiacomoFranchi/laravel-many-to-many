0d7ceb14506e4b6010bc4ccc4f2036d2d2aa5d64
GiacomoFranchi <giacomo.franchi.97@gmail.com>
2024-01-30 16:26:43 +0100

    add: message for tecnologies in Update&storeRequest

--- app/Http/Requests/StoreProjectRequest.php
+++ app/Http/Requests/StoreProjectRequest.php
@@ -38,7 +38,8 @@ class StoreProjectRequest extends FormRequest
             'title.min' => 'Il titolo può avere minimo :min caratteri',
             'title.max' => 'Il titolo può avere al massimo :max caratteri',
             'cover_image.image' =>'formato errato',
-            'cover_image.max' =>'dimensione eccessiva'
+            'cover_image.max' =>'dimensione eccessiva',
+            'tecnologies' => 'Non modificare il valore di Tecnologies furbetto...'
         ];
     }
 }
